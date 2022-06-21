ab26ea336f0d0f071728b64a20303b091754a071
Keks <keks@htmlacademy.ru>
2022-06-21 16:14:09 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,9 @@
+import './util.js';
+import './data_base.js';
+import './data.js';
+
+import {getRandomNumber,getRandomCoordinate,getArrValue} from './util.js';
+import {types,time,features,photos} from './data_base.js';
+import {getAvatar,getOffer,getLocation} from './data.js';
+
+
